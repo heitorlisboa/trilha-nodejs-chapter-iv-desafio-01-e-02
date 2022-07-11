19fed0629664204e6381f702d9bab911f2a39b28
heitorlisboa <heitor01101000@gmail.com>
2022-07-11 13:18:07 -0300

    fix: update `AppError` to extend `Error`
    `AppError` was not considered an error so I was unable to
    use Jest's `expect().toThrow()` passing this error as argument

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,9 +1,10 @@
-export class AppError {
-  readonly message: string;
-
-  readonly statusCode: number;
+export class AppError extends Error {
+  public readonly message: string;
+  public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
+    super(message);
+    this.name = 'AppError';
     this.message = message;
     this.statusCode = statusCode;
   }
