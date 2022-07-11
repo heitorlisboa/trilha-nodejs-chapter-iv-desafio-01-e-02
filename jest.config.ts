b845b5ae384a86c1eef84ab5cea50d1d0b81e7db
heitorlisboa <heitor01101000@gmail.com>
2022-07-11 12:51:50 -0300

    chore: setup jest for unit testing

---
+++ jest.config.ts
@@ -0,0 +1,44 @@
+export default {
+  // Stop running tests after `n` failures
+  bail: true,
+
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    "src/**/in-memory/*Repository.ts",
+    "src/**/*UseCase.ts",
+  ],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // modulePathIgnorePatterns: [],
+
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest",
+
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  // setupFiles: [],
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+  testEnvironment: "node",
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/*.spec.ts"],
+};
